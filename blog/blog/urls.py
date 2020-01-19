1acd819e7eb0a102f4f2c07716c1fdd4844c54fa
opsxin <haoxin0880@gmail.com>
2020-01-19 17:00:16 +0800

    调整 HTML 元数据

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.views import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("subject.urls")),
+    path('favicon.ico', serve, {'path': 'subject/img/favicon.ico'}),
 ]
