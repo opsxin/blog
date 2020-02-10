c5fa4ed34dae0c8a65c9704ba85de02967b03b6e
opsxin <haoxin0880@gmail.com>
2020-02-10 21:12:18 +0800

    静态文件通过 Nginx 方式

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.views import serve
+# from django.contrib.staticfiles.views import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("subject.urls")),
-    path('favicon.ico', serve, {'path': 'subject/img/favicon.ico'}),
+    # path('favicon.ico', serve, {'path': 'subject/img/favicon.ico'}),
 ]
