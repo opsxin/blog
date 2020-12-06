75efa760bbcc559fceeb386deb35c8d1ce8af991
opsxin <haoxin0880@gmail.com>
2020-12-06 15:31:30 +0800

    Paste: 增加 web 界面

--- blog/paste/urls.py
+++ blog/paste/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = "paste"
 urlpatterns = [
+        path('paste/', views.paste, name="paste"),
         path('paste/<str:name>', views.paste_upload, name="paste_upload"),
         path('paste/<str:name>/<int:id>', views.paste_show, name="paste_show"),
 ]
