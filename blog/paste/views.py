db992483fc864a3c2c4d44dc46ec7b442d040825
hsin <13961132+opsxin@users.noreply.github.com>
2020-12-06 15:32:54 +0800

    Paste: 增加 web 界面 (#63)

--- blog/paste/views.py
+++ blog/paste/views.py
@@ -4,28 +4,55 @@ from .models import Paste
 from datetime import datetime, timedelta
 from django.urls import reverse
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
 
-# Create your views here.
+code_classes = ["plaintext", "bash", "c++", "c#", "css", "dockerfile", "go", "html", "java",
+                "javascript", "json", "markdown", "php", "python", "sql", "typescript", "xml", "yaml"]
+
+
+def paste(request):
+    if request.method == "GET":
+        return render(request, 'paste/paste.html')
+    if request.method == "POST":
+        name = request.POST.get("name")
+
+        code_class = request.POST.get("code_class")
+        if code_class not in code_classes:
+            code_class = "nohighlight"
+
+        valid_day = request.POST.get("validity")
+        if int(valid_day) <= 0 or int(valid_day) > 30:
+            valid_day = 30
+
+        msg = request.POST.get("message")
+
+        p = Paste.objects.create(name=name, message=bleach.clean(
+            msg, tags=[], strip_comments=False), valid_day=valid_day, code_class=code_class.lower())
+        url = reverse('paste:paste_show', kwargs={'name': name, 'id': p.pk})
+
+        return redirect(request.build_absolute_uri(url))
+    return redirect(reverse("subject:index"))
+
+
 @require_http_methods(["POST"])
 @csrf_exempt
 def paste_upload(request, name):
     try:
         paste = json.loads(request.body.decode('utf-8'))
-        valid_day = paste.get("validity", 3)
-        code_class = paste.get("class", "nohighlight")
+        valid_day = paste.get("validity", 1)
+        code_class = paste.get("code_class", "nohighlight")
         msg = paste.get("message")
 
-        if len(code_class) > 15:
+        if code_class not in code_classes:
             code_class = "nohighlight"
-        if valid_day > 31:
-            valid_day = 31
+        if int(valid_day) <= 0 or int(valid_day) > 30:
+            valid_day = 30
 
         p = Paste.objects.create(name=name, message=bleach.clean(
-            msg), valid_day=valid_day, code_class=code_class.lower())
+            msg, tags=[], strip_comments=False), valid_day=valid_day, code_class=code_class.lower())
         url = reverse('paste:paste_show', kwargs={'name': name, 'id': p.pk})
 
         return HttpResponse(request.build_absolute_uri(url))
@@ -42,9 +69,4 @@ def paste_show(requests, name, id):
     if datetime.now() > varidity:
         return HttpResponse("Link Expired!")
     else:
-        html = '''<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.4.0/build/styles/atom-one-light.min.css">
-<script src="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.4.0/build/highlight.min.js"></script>
-<script>hljs.initHighlightingOnLoad();</script>
-<pre><code class="{}" style="font-size: 1.6em;">{}</code></pre>'''.format(p.code_class, p.message)
-
-        return HttpResponse(html)
+        return render(requests, 'paste/paste_show.html', {"contents": p})
